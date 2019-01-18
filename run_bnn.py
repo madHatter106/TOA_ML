20f364edbbaed0b0506a8b253bc484c445085fc4
madHatter106 <jkomd.283.2112@gmail.com>
2019-01-18 14:44:55 -0500

    bnn fitting script

---
+++ run_bnn.py
@@ -0,0 +1,47 @@
+"""Runs ARD BNN"""
+
+import pickle
+from theano import shared
+from pymc_models import PyMCModel
+from pymc_models import bayes_nn_model_ARD_1HL_halfCauchy_hyperpriors
+
+
+if __name__ == "__main__":
+
+    # load datasets
+    with open('./pickleJar/AphiTrainTestSpliDataSets.pkl', 'rb') as fb:
+        datadict = pickle.load(fb)
+    X_s_train = datadict['x_train_s']
+    y_train = datadict['y_train']
+    X_s_test = datadict['x_test_s']
+    y_test = datadict['y_test']
+
+    bands = [411, 443, 489, 510, 555, 670]
+    model_dict=dict.fromkeys(bands)
+    for band in bands:
+        model_dict[band] = dict.fromkeys(['model', 'trace', 'ppc_train', 'ppc_test'])
+
+    # create theano shared variable
+    X_shared = shared(X_s_train.values)
+
+    # Fitting aphi411 model:
+    # Instantiate PyMC3 model with bnn likelihood
+    for band in bands:
+        bnn_ = PyMCModel(bayes_nn_model_ARD_1HL_halfCauchy_hyperpriors,
+                            X_shared, y_train['log10_aphy%d' %band], n_hidden=4)
+        bnn_.model.name = 'bnn_HL4_%d' %band
+        bnn_.fit(n_samples=2000, cores=1, chains=4, tune=10000,
+                    nuts_kwargs=dict(target_accept=0.95))
+        X_shared.set_value(X_s_train.values)
+        ppc_train_ = bnn_.predict(likelihood_name='likelihood')
+        X_shared.set_value(X_s_test.values)
+        ppc_test_ = bnn_.predict(likelihood_name='likelihood')
+
+        model_dict[band]['model'] = bnn_.model
+        model_dict[band]['trace'] = bnn_.trace_
+        model_dict[band]['ppc_train'] = ppc_train_
+        model_dict[band]['ppc_test'] = ppc_test_
+
+        with open('./pickleJar/bnn_model_dict', 'wb') as fb:
+            pickle.dump(model_dict, fb, protocol=pickle.HIGHEST_PROTOCOL)
+    
